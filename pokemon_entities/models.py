22f4f6b04bb9acea58d48d8dffb4db66ad7057b5
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-03-02 18:07:35 +0300

    delete id from models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,6 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     """Покемон"""
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200,
                              verbose_name='Имя')
     description = models.TextField(blank=True,
