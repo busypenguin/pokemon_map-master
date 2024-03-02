f3add2d2cc60f112e2d08a66ff85323ebc032690
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-03-02 12:19:55 +0300

    change the name of the Fields to Russian

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,16 +4,26 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
+    """Покемон"""
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
-    photo = models.ImageField(blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200,
+                             blank=True,
+                             verbose_name='Имя')
+    description = models.TextField(blank=True,
+                                   verbose_name='Описание')
+    photo = models.ImageField(blank=True,
+                              verbose_name='Фото')
+    title_en = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name='Имя на английском')
+    title_jp = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name='Имя на японском')
     previous_evolution = models.ForeignKey('Pokemon',
                                            on_delete=models.CASCADE,
                                            null=True, blank=True,
-                                           related_name='next_evolution')
+                                           related_name='next_evolution',
+                                           verbose_name='Из кого эволюционировал')
 
     def __str__(self):
         if self.title:
@@ -21,13 +31,34 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    latitude = models.FloatField(null=True, blank=True)  #широта
-    longitude = models.FloatField(null=True, blank=True)  #долгота
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    strenght = models.IntegerField(null=True, blank=True)
-    defence = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    """Сущность покемона"""
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE,
+                                verbose_name='Покемон')
+    latitude = models.FloatField(null=True,
+                                 blank=True,
+                                 verbose_name='Широта')
+    longitude = models.FloatField(null=True,
+                                  blank=True,
+                                  verbose_name='Долгота')
+    appeared_at = models.DateTimeField(null=True,
+                                       blank=True,
+                                       verbose_name='Появился')
+    disappeared_at = models.DateTimeField(null=True,
+                                          blank=True,
+                                          verbose_name='Исчез')
+    level = models.IntegerField(null=True,
+                                blank=True,
+                                verbose_name='Уровень')
+    health = models.IntegerField(null=True,
+                                 blank=True,
+                                 verbose_name='Здоровье')
+    strenght = models.IntegerField(null=True,
+                                   blank=True,
+                                   verbose_name='Атака')
+    defence = models.IntegerField(null=True,
+                                  blank=True,
+                                  verbose_name='Защита')
+    stamina = models.IntegerField(null=True,
+                                  blank=True,
+                                  verbose_name='Выносливость')
